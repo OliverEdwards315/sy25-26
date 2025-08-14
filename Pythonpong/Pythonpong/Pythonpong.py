609675d16b6b1c657062bbd17d8ac430c6c9d593
Oliver <ojedw545@learner.dvusd.org>
2025-08-14 09:42:43 -0700

    Pong game

---
+++ Pythonpong/Pythonpong/Pythonpong.py
@@ -0,0 +1,26 @@
+# Import required library
+import turtle
+
+# Create screen
+sc = turtle.Screen()
+sc.title("Pong game")
+sc.bgcolor("black")
+sc.setup(width=800, height=400)
+
+
+# Left paddle
+left_pad = turtle.Turtle()
+left_pad.speed(0)
+left_pad.shape("square")
+left_pad.color("white")
+left_pad.shapesize(stretch_wid=6, stretch_len=2)
+left_pad.penup()
+left_pad.goto(-350, 0)
+# Right paddle
+right_pad = turtle.Turtle()
+right_pad.speed(0)
+right_pad.shape("square")
+right_pad.color("white")
+right_pad.shapesize(stretch_wid=6, stretch_len=2)
+right_pad.penup()
+right_pad.goto(350, 0)
