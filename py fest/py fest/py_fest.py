513b0e716d8e9687ca5e154b7f60fce534d29608
Oliver <ojedw545@learner.dvusd.org>
2026-01-12 14:02:09 -0700

    Version 1

--- py fest/py fest/py_fest.py
+++ py fest/py fest/py_fest.py
@@ -1,1 +1,12 @@
+print("/n---Py-Fest 2026 stage Manager ---")
+print("1. View Lineup & total time")
+print("2. Add a new band")
+print("3. Move first Band to End (late arival)")
+print("4. Remove a band by name")
+print("5. Move band by name to specific position")
+print("6. Exit")
+
+choice = input("enter your choice (1-6): ")
+if choice == "1":
+        print("\n--- Current Lineup ---")
 
