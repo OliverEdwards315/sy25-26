91629efe6f654261c5351e81ca3be77c358aa545
Oliver <ojedw545@learner.dvusd.org>
2026-01-13 13:54:02 -0700

    Tuesday update

--- py fest/py fest/py_fest.py
+++ py fest/py fest/py_fest.py
@@ -6,7 +6,30 @@ print("4. Remove a band by name")
 print("5. Move band by name to specific position")
 print("6. Exit")
 
-choice = input("enter your choice (1-6): ")
-if choice == "1":
-        print("\n--- Current Lineup ---")
 
+lineup = [
+    {"name": "band name", "duration": 10},
+    {"name": "band name", "duration": 10},
+    {"name": "band name", "duration": 10},
+    {"name": "band name", "duration": 10},
+    ]
+
+
+while True:
+    
+    choice = input("enter your choice (1-6): ")
+    
+
+    if choice == "1":
+        print("\n--- Current Lineup ---")
+        print(lineup)
+
+
+
+    elif choice == "2":
+        new_band = input("Enter the name of the new band: ")
+        print(f"{new_band} has been added to the lineup.")
+
+    elif choice == "3":
+        band_name = int(input("Enter the name of the band to move from front to end: "))
+        print(f"{band_name} has been moved to the end of the lineup.")
